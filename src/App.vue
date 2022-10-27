d75ebb7bb6e133bf9d3603ddadd552145b0a8a84
RageKage <99547837+RageKage@users.noreply.github.com>
2022-10-27 14:22:30 -0500

    commit- before delete update

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,61 @@
 <template>
-    <div id="app">
-      <new-student-form></new-student-form>
-      <student-table></student-table>
-      <student-message></student-message>
-    </div>
+  <div id="app">
+    <new-student-form v-on:student-added="newStudentAdded"></new-student-form>
+    <student-table
+      v-bind:students="students"
+      v-on:student-arrived-or-left="studentArrivedOrLeft"
+    ></student-table>
+    <student-message :student="mostRecentStudents"></student-message>
+  </div>
 </template>
 
 <script>
-
-import NewStudentFormVue from './components/NewStudentForm.Vue';
-import StudentMessageVue from './components/StudentMessage.Vue';
-import StudentTableVue from './components/StudentTable.Vue';
-
-
+import NewStudentForm from "./components/NewStudentForm.vue";
+import StudentMessage from "./components/StudentMessage.vue";
+import StudentTable from "./components/StudentTable.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    NewStudentFormVue,
-    StudentMessageVue,
-    StudentTableVue
-  }
-}
+    NewStudentForm,
+    StudentMessage,
+    StudentTable,
+  },
+  data() {
+    return {
+      students: [],
+      mostRecentStudents: {}
+    };
+  },
+  methods: {
+    newStudentAdded(student) {
+      this.students.push(student);
+
+      this.students.sort(function (s1, s2) {
+        return s1.name.toUpperCase() < s2.name.toUpperCase() ? -1 : 1;
+      });
+    },
+    studentArrivedOrLeft(student, present){
+        // find the student in array of students
+        // update the present Attribute
+
+        let updateStudent = this.students.find(function(s){
+          if(s.name === student.name && s.starID === student.starID) {
+            return true
+          }
+        
+        })
+        if (updateStudent) {
+          updateStudent.present = present
+          this.mostRecentStudents = updateStudent
+          console.log(updateStudent)
+        }
+    }
+  },
+
+
+};
 </script>
-
 <style>
-
+@import "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css";
 </style>
---
+++ src/components/StudentRow.vue
@@ -0,0 +1,41 @@
+<template>
+  <tr v-bind:class="{ present: student.present, absent: !student.present }">
+    <td>{{ student.name }}</td>
+    <td>{{ student.starID }}</td>
+    <td>
+      <input
+        type="checkbox"
+        v-bind:checked="student.present"
+        v-on:change="arrivedOrLeft(student, $event.target.checked)"
+      />
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "StudentRow",
+  emits: ["student-arrived-or-left"],
+  props: {
+    student: Object,
+  },
+  methods: {
+    arrivedOrLeft(student, present) {
+      this.$emit("student-arrived-or-left", student, present);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.present {
+  color: gray;
+  font-style: italic;
+}
+
+.absent {
+  color: rgb(0, 0, 0);
+  font-weight: bold;
+  background: coral;
+}
+</style>
