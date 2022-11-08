2ad9dc4f19eaa94f63958664e5b5324c1f371c51
RageKage <99547837+RageKage@users.noreply.github.com>
2022-11-08 11:28:29 -0600

    commit - hope to fixed it

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   // replace with the name of your github repository - just the name, not the https://github.com/you/ part
-    ? '/student-sign-in/student'
+    ? '/student-sign-in/'
     : '/',
     outputDir: "docs"
 }
